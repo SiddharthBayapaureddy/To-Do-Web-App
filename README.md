d712838c4fb1dae80ee994f9f8ecb562657380c5
Siddharth <siddharthbayapureddy@gmail.com>
2024-11-01 00:30:57 +0530

    Added a README File, Add & Del functionality

---
+++ README.md
@@ -0,0 +1,4 @@
+# Welcome to my ToDo Web Applicatio
+
+### I'm just working on building a web app on a simple todo list function capable program, using python and streamlit library. Hopefully it works!! 
+## :)  
--- web.py
+++ web.py
@@ -5,17 +5,31 @@ to_dos = function.get_todos()
 
 def add_todo():
     todo = st.session_state['new_todo'] + '\n'
-    
+    if todo.strip() == '':
+        print('.')
+    else:
+        to_dos.append(todo)
+        function.write_todos(to_dos)
+        st.session_state['new_todo'] = ''
+        
+
 
 
 st.title('To-Do Web App')
 st.subheader('This is my todo app')
 st.write('Siddharth B')
 
-for todo in to_dos:
-    st.checkbox(todo)
+to_dos = function.get_todos()
+
+for indix,todo in enumerate(to_dos):
+    checkbox = st.checkbox(todo,key=indix)
+    if checkbox:
+        to_dos.pop(indix)
+        function.write_todos(to_dos)
+        st.rerun()
+
 
 st.text_input(label='Enter a To Do',
-               placeholder='Add a new todo',
+               placeholder='Add a new todo...',
                key='new_todo',
                on_change=add_todo)    
