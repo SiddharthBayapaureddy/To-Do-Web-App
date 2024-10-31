aaf4296a8b805197d0e451d0d506d5a3cb819da4
Siddharth <siddharthbayapureddy@gmail.com>
2024-11-01 00:19:06 +0530

    Commit-2

--- web.py
+++ web.py
@@ -3,6 +3,11 @@ from Functions import function
 
 to_dos = function.get_todos()
 
+def add_todo():
+    todo = st.session_state['new_todo'] + '\n'
+    
+
+
 st.title('To-Do Web App')
 st.subheader('This is my todo app')
 st.write('Siddharth B')
@@ -10,4 +15,7 @@ st.write('Siddharth B')
 for todo in to_dos:
     st.checkbox(todo)
 
-st.text_input(label='Enter a To Do', placeholder='Add a new todo')    
+st.text_input(label='Enter a To Do',
+               placeholder='Add a new todo',
+               key='new_todo',
+               on_change=add_todo)    
